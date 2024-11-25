b56bd00560d532043ccb3400ad2b492cbeb3b639
GuillermoFA <guillermo.fuentes01@alumnos.ucn.cl>
2024-11-24 23:44:14 -0300

    fix responsive in tablet

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -58,7 +58,7 @@ function Home() {
       {isLoading ? (
         <p>Loading...</p>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 mt-4">
+        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
           {filteredCharacters.map((character) => (
             <CharacterCard key={character.id} character={character} />
           ))}
