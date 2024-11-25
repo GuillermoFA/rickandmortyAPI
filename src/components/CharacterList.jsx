936a1cb1f95dc785b04668e624a18ecac6addbfd
GuillermoFA <guillermo.fuentes01@alumnos.ucn.cl>
2024-11-24 23:13:28 -0300

    fix navbar

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -3,7 +3,7 @@ import CharacterCard from "./CharacterCard";
 
 function CharacterList({ characters }) {
   return (
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 sm:grid-cols-1">
       {characters.map((character) => (
         <CharacterCard key={character.id} character={character} />
       ))}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,6 @@ function Navbar() {
   return (
     <nav className="bg-gray-800 text-white p-4 shadow">
       <div className="container mx-auto flex items-center justify-between">
-        {/* Logo */}
         <div className="flex items-center space-x-3">
           <img
             src="https://rickandmortyapi.com/icons/icon-512x512.png"
